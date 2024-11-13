749dccada07b73f05e308099fece757bed945b93
Late-Smoke <3136906856@qq.com>
2024-11-14 03:06:43 +0800

    <feat>(scr/utils):<新增axios封装>

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,17 @@ import router from './router'
 import 'normalize.css'
 import '@/assets/styles/common.scss'
 import '@/utils/request'
+import {post} from "@/utils/api.ts";
+import {put} from "@/utils/api.ts";
+import {get} from "@/utils/api.ts";
+import {deleted} from "@/utils/api.ts";
 
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
+Vue.prototype.post = post;
+Vue.prototype.put = put;
+Vue.prototype.get = get;
+Vue.prototype.deleted = deleted;
+ 
