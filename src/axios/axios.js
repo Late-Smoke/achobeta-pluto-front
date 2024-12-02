fd54be540616e1c1e286219634228bd997768444
Late-Smoke <3136906856@qq.com>
2024-12-02 08:16:34 +0800

    chore

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //axios实例
 const apiClient = axios.create({
     // baseURL: 'http://117.72.97.101:8085',
-    timeout: 30000, // 请求超时时间为30秒
+    timeout: 30000, // 请求超时时间为10秒
     headers: { 'Content-Type': 'application/json' },
 });
 export default apiClient;
